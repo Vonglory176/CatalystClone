ce652eb591ca926896dc8952897e76045fec8770
Vonglory176 <SkylerGConley@gmail.com>
2023-12-07 13:54:10 -0800

    Initial Commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import catalystLogo from "../assets/logo-catalyst2_450x.webp"
+
+export default function Header() {
+    return (
+        <header>
+            <img src={catalystLogo}/>
+        </header>
+    )
+}
