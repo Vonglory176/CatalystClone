1d08deb4428d16ea49c81dd0832d63a418494e86
Vonglory176 <SkylerGConley@gmail.com>
2023-12-09 16:08:52 -0800

    First stab at MailListSignUp

---
+++ src/components/MailListSignUp.jsx
@@ -0,0 +1,11 @@
+export default function MailListSignUp() {
+    return (
+        <div className="mailListSignUp">
+            <form action="" target="_blank">
+                <label htmlFor="mailListSignUp-email">Subscribe to our mailing list for exclusive access to new releases and sales</label>
+                <input type="email" name="email" className="email" placeholder="Email Address" required/>
+                <input type="submit" value="Subscribe" name="subscribe" className="button"/>
+            </form>
+        </div>
+    )
+}
