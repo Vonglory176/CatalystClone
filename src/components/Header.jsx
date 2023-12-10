58cff17eb97a9c4f771eaeff2615fdc7d98dd311
Vonglory176 <SkylerGConley@gmail.com>
2023-12-09 16:12:36 -0800

    Basic header done. Needs code for sidebar + cart

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,25 @@ import catalystLogo from "../assets/logo-catalyst2_450x.webp"
 
 export default function Header() {
     return (
-        <header>
-            <img src={catalystLogo}/>
+        <header className="header">
+            <div className="header__upper">
+                <div className="menu-btn">
+                    <i className="fa-solid fa-bars menu-btn__burger"></i>
+                    {/* <i className="fa-solid fa-xmark"></i> */}
+                </div>
+
+                <div className="company-logo">
+                    <a href="!#" className="company-logo__link">
+                        <img src={catalystLogo} className="company-logo__image" alt="Catalyst Game Labs Logo"/>
+                    </a>
+                </div>
+
+                <div className="store-btn">
+                    <i className="fa-solid fa-cart-shopping menu-btn__cart"></i>
+                    {/* <i className="fa-solid fa-magnifying-glass"></i> */}
+                </div>
+            </div>
+            {/* <div className="header-sticky"></div> */}
         </header>
     )
 }
