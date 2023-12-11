1b47b93eb6b1b78de34250911020b17dfaea1e78
Vonglory176 <SkylerGConley@gmail.com>
2023-12-10 17:31:30 -0800

    ToDo comments creation

--- src/App.jsx
+++ src/App.jsx
@@ -14,3 +14,29 @@ function App() {
 }
 
 export default App
+
+/*
+Major breakpoints at: 600px, 960px, 1280px, 1920px
+
+TODO
+-------------------
+Right now!
+------
+Add hover animations/transitions
+
+Later!
+------
+Clean up SCSS
+Finish Header
+Add Header Drawer
+Get rid of extra divs?
+Fix email styling
+Fix font styling in Product Card
+Fix Collection-Link styling in Product card
+
+(General)
+Add Titles to anchors
+Check out "lazysizes" & data-src
+
+-------------------
+*/
