cd560fb60a6480c872fabbf729c695cb283b5ca2
Vonglory176 <SkylerGConley@gmail.com>
2023-12-15 18:33:22 -0800

    Code cleanup & Link setup

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export default function Footer() {
     return (
         <footer className="footer">
@@ -8,16 +10,16 @@ export default function Footer() {
                             <h4 className="h1 footer__section-title">Navigation</h4>
                             <ul className="footer__list">
                                 <li className="footer__list-item">
-                                    <a href="!#">Cart</a>
+                                    <Link to={"/cart"} className={""}>Cart</Link>
                                 </li>
                                 <li className="footer__list-item">
-                                    <a href="!#">My Account</a>
+                                    <Link to={"/account/login"} className={""}>My Account</Link>
                                 </li>
                                 <li className="footer__list-item">
-                                    <a href="!#">Logout</a>
+                                    <Link to={"/account/logout"} className={""}>Logout</Link>
                                 </li>
                                 <li className="footer__list-item">
-                                    <a href="!#">Contact</a>
+                                    <Link to={"/contact"} className={""}>Contact</Link>
                                 </li>
                             </ul>
                         </section>
@@ -25,35 +27,30 @@ export default function Footer() {
                     <div className="flex__item">
                         <section className="footer__section">
                             <h4 className="h1 footer__section-title">Follow Us</h4>
-                            <ul className="social-icons">
-                                <li>
-                                    <a href="!#">
-                                        <i className="fa-brands fa-facebook-f fa-lg"></i>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="!#">
-                                        <i className="fa-brands fa-twitter fa-lg"></i>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="!#">
-                                        <i className="fa-brands fa-instagram fa-lg"></i>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="!#">
-                                        <i className="fa-brands fa-tumblr fa-lg"></i>
-                                    </a>
-                                </li>
-                            </ul>
+                            <div className="social-icons">                                
+                                <a href="!#">
+                                    <i className="fa-brands fa-facebook-f fa-lg"></i>
+                                </a>                            
+                            
+                                <a href="!#">
+                                    <i className="fa-brands fa-twitter fa-lg"></i>
+                                </a>                            
+                            
+                                <a href="!#">
+                                    <i className="fa-brands fa-instagram fa-lg"></i>
+                                </a>                            
+                            
+                                <a href="!#">
+                                    <i className="fa-brands fa-tumblr fa-lg"></i>
+                                </a>                                
+                            </div>
                         </section>
                     </div>
                 </div>
             </div>
             <div className="footer__copyright">
                 <div className="page-width">            
-                    <small>2023, MOCK RECREATION of the <a href="https://store.catalystgamelabs.com">Catalyst Game Labs Store</a> by Skyler Conley. <a href="https://activatorstudios.com/">Original design by Activator Studios.</a></small>
+                    <small>2023, <a href="https://github.com/Vonglory176/CatalystClone">MOCK RECREATION</a> of the <a href="https://store.catalystgamelabs.com">Catalyst Game Labs Store</a> by Skyler Conley. <a href="https://activatorstudios.com/">Original design by Activator Studios.</a></small>
                 </div>
             </div>
         </footer>
