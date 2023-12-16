e9fbaae6fb5d364d1860fba53c50fcf6bfa29e94
Vonglory176 <SkylerGConley@gmail.com>
2023-12-15 18:34:57 -0800

    Typo fix in email input

--- src/components/MailListSignUp.jsx
+++ src/components/MailListSignUp.jsx
@@ -3,7 +3,7 @@ export default function MailListSignUp() {
         <div className="mailListSignUp">
             <form action="" target="_blank">
                 <label htmlFor="mailListSignUp-email">Subscribe to our mailing list for exclusive access to new releases and sales</label>
-                <input type="email" name="email" className="email" placeholder="Email Address" required/>
+                <input type="email" id="mailListSignUp-email" name="email" autoComplete="email" className="email" placeholder="Email Address" required/>
                 <input type="submit" value="Subscribe" name="subscribe" className="button"/>
             </form>
         </div>
