7919e93f3dd7fef7333cf84cb4c898a7cdda4c02
Vonglory176 <SkylerGConley@gmail.com>
2023-12-17 14:59:46 -0800

    Basic page for 404 Routing errors

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+export default function NotFound() {
+    return (
+        <div id="NotFound-Container">
+            <h1>PAGE NOT FOUND</h1>
+            <p className="notFound__message">The page you requested does not exist.</p>
+            <hr />
+            <p className="notFound__continue">
+                <Link to={"/collections/all"} className={"btn"}>Continue shopping</Link> {/* Note "All" */}
+            </p>
+        </div>
+    )
+}
