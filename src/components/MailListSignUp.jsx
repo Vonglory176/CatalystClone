a0e8b1da238f3c654cee4ca63028505a34e02bee
Vonglory176 <SkylerGConley@gmail.com>
2023-12-20 23:15:54 -0800

    Button class swapped to btn

--- src/components/MailListSignUp.jsx
+++ src/components/MailListSignUp.jsx
@@ -4,7 +4,7 @@ export default function MailListSignUp() {
             <form action="" target="_blank">
                 <label htmlFor="mailListSignUp-email">Subscribe to our mailing list for exclusive access to new releases and sales</label>
                 <input type="email" id="mailListSignUp-email" name="email" autoComplete="email" className="email" placeholder="Email Address" required/>
-                <input type="submit" value="Subscribe" name="subscribe" className="button"/>
+                <input type="submit" value="Subscribe" name="subscribe" className="btn"/> {/* className={"button"} */}
             </form>
         </div>
     )
