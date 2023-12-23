8ec357b17e7ea8fc1027a71165f585b9519127f7
Vonglory176 <SkylerGConley@gmail.com>
2023-12-22 23:36:51 -0800

    First stab at data file for products

---
+++ src/data/products.jsx
@@ -0,0 +1,38 @@
+const products = {
+    battletech: [
+        {   
+            universe:"",
+            name:"",
+            price:"",
+            version:"",
+            format:"",
+            images: {
+                primary:"",
+                secondary: [],
+            },
+            type: [""],
+            description:"",
+        },
+        {   
+            name:"",
+            price:"",
+            version:"",
+            format:"",
+            images: {
+                primary:"",
+                secondary: [],
+            },
+            type: [""],
+            description:"",
+        },
+    ],
+    
+    shadowrun: [
+    ],
+
+    other: [
+
+    ]
+}
+
+export default products
