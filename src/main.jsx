e13938947dc9c733fbdebc24100019ff1569a4bc
Vonglory176 <SkylerGConley@gmail.com>
2023-12-28 19:32:40 -0800

    Added Provider/Store wrap for App

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import "./style/index.css"
+import { Provider } from 'react-redux'
+import store from './store/index.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename={"/"}> {/*CatalystClone*/}
-      <App/>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter basename={"/"}> {/*CatalystClone*/}
+        <App/>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
 )
