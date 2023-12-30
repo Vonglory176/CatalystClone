f1406225c8ea2542905369044957291e2fa51a4b
Vonglory176 <SkylerGConley@gmail.com>
2023-12-29 17:39:55 -0800

    Added a couple mock database products

---
+++ src/data/productList.jsx
@@ -0,0 +1,123 @@
+const products = {
+    battletech: {
+        0:{   
+            id:"battletech-clan-invasion",
+            universe: "BattleTech",
+            name: "Clan Invasion",
+
+            productTypes: "",
+            tags: "",
+
+            sharedImages: {
+                0: {
+                    name:"BackCover",
+                    alt:"Back of the box",
+                    link:"https://firebasestorage.googleapis.com/v0/b/catalystclonedb.appspot.com/o/products%2Fbattletech%2Fclan-invasion%2FBoxSpread.webp?alt=media&token=e65c26ca-db3b-4a9d-9c8f-591146bbd877"
+                },
+                
+                1: {
+                    name:"BoxSpread",
+                    alt:"Spread contents of the box",
+                    link:"https://firebasestorage.googleapis.com/v0/b/catalystclonedb.appspot.com/o/products%2Fbattletech%2Fclan-invasion%2FBoxSpread.webp?alt=media&token=e65c26ca-db3b-4a9d-9c8f-591146bbd877"
+                }
+            },
+            
+            variants: {
+                
+                type:"Version",
+                
+                choices: {
+                    0: {
+                        name: "Unlimited",
+                        price: 49.99,
+                        images: {
+                            0: {
+                                name:"UnlimitedBox",
+                                alt:"The Unlimited box",
+                                link:"https://firebasestorage.googleapis.com/v0/b/catalystclonedb.appspot.com/o/products%2Fbattletech%2Fclan-invasion%2FUnlimitedBox.webp?alt=media&token=135bd3a8-feb6-446c-b9a7-2e0e7a052e76"
+                            }
+                        }
+                    },
+                    1: {
+                        name: "Kickstarter",
+                        price: 49.99,
+                        images: {
+                            0: {
+                                name:"BoxSpread",
+                                alt:"The Kickstarter box",
+                                link:"https://firebasestorage.googleapis.com/v0/b/catalystclonedb.appspot.com/o/products%2Fbattletech%2Fclan-invasion%2FKickStarterBox.webp?alt=media&token=2d3dee8a-3f0f-46f0-8142-00837d357a5b"
+                            }
+                        }
+                    }
+                }
+            },
+
+            description: "<div className='product-description'>Hello! This is a test.</div>"
+            ,
+        },
+        1:{   
+            id:"battletech-clan-invasion2",
+            universe: "BattleTech",
+            name: "Clan Invasion",
+
+            productTypes: "",
+            tags: "",
+
+            sharedImages: {
+                0: {
+                    name:"BackCover",
+                    alt:"Back of the box",
+                    link:"https://firebasestorage.googleapis.com/v0/b/catalystclonedb.appspot.com/o/products%2Fbattletech%2Fclan-invasion%2FBoxSpread.webp?alt=media&token=e65c26ca-db3b-4a9d-9c8f-591146bbd877"
+                },
+                
+                1: {
+                    name:"BoxSpread",
+                    alt:"Spread contents of the box",
+                    link:"https://firebasestorage.googleapis.com/v0/b/catalystclonedb.appspot.com/o/products%2Fbattletech%2Fclan-invasion%2FBoxSpread.webp?alt=media&token=e65c26ca-db3b-4a9d-9c8f-591146bbd877"
+                }
+            },
+            
+            variants: {
+                
+                type:"Version",
+                
+                choices: {
+                    0: {
+                        name: "Unlimited",
+                        price: 49.99,
+                        images: {
+                            0: {
+                                name:"UnlimitedBox",
+                                alt:"The Unlimited box",
+                                link:"https://firebasestorage.googleapis.com/v0/b/catalystclonedb.appspot.com/o/products%2Fbattletech%2Fclan-invasion%2FUnlimitedBox.webp?alt=media&token=135bd3a8-feb6-446c-b9a7-2e0e7a052e76"
+                            }
+                        }
+                    },
+                    1: {
+                        name: "Kickstarter",
+                        price: 49.99,
+                        images: {
+                            0: {
+                                name:"BoxSpread",
+                                alt:"The Kickstarter box",
+                                link:"https://firebasestorage.googleapis.com/v0/b/catalystclonedb.appspot.com/o/products%2Fbattletech%2Fclan-invasion%2FKickStarterBox.webp?alt=media&token=2d3dee8a-3f0f-46f0-8142-00837d357a5b"
+                            }
+                        }
+                    }
+                }
+            },
+
+            description: "<div className='product-description'>Hello! This is a test.</div>"
+            ,
+        },
+    },
+    
+    shadowrun: [
+    ],
+
+    other: [
+
+    ]
+}
+
+export default products
