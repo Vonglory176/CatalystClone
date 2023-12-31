92da1b20a3c6b7d6d25e7d67bc626f975ed592e5
Vonglory176 <SkylerGConley@gmail.com>
2023-12-30 16:50:45 -0800

    Firebase slice import and reducer inclusion

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit"
 import authSlice from "./auth-slice"
+import firebaseSlice from "./firebase-slice"
 
 const store = configureStore({
     reducer: {
         auth: authSlice.reducer,
+        firebase: firebase-slice.reducer
     }
 })
 
