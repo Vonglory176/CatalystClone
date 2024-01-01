0f2388abbae88be4f1e0d049877d9cb6319daaf4
Vonglory176 <SkylerGConley@gmail.com>
2023-12-31 20:12:25 -0800

    Cleaned up code & flushed out db productList query

--- src/store/firebase-slice.jsx
+++ src/store/firebase-slice.jsx
@@ -1,60 +1,99 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { getDatabase, ref, set, get, child, update, remove } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+import { ref, set, get, child, update, remove } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+import db from '../firebase';
 
-const db = getDatabase();
+export const fetchProducts = createAsyncThunk(
+  'firebase/fetchProducts',
+  async ( _, thunkAPI) => {
+    const productsRef = ref(db, 'products')
+    const snapshot = await get(productsRef) //Getting ProductList from Firebase
+    console.log(snapshot)
+    if (snapshot.exists()) return snapshot.val()
 
-export const insertData = createAsyncThunk(
-  'firebase/insertData',
-  async (data, thunkAPI) => {
-    // Insert data into Firebase
-    // You can dispatch other actions within this function
-    // Return the result or throw an error
+    else throw new Error("No products avalible")
   }
-);
-
-export const selectData = createAsyncThunk(
-  'firebase/selectData',
-  async (data, thunkAPI) => {
-    // Select data from Firebase
-  }
-);
-
-export const updateData = createAsyncThunk(
-  'firebase/updateData',
-  async (data, thunkAPI) => {
-    // Update data in Firebase
-  }
-);
-
-export const deleteData = createAsyncThunk(
-  'firebase/deleteData',
-  async (data, thunkAPI) => {
-    // Delete data from Firebase
-  }
-);
+)
 
 const firebaseSlice = createSlice({
   name: 'firebase',
-  initialState: {},
-  reducers: {},
-  extraReducers: (builder) => {
+  initialState: {
+    productList: null,
+    status: 'idle',
+    error: null,
+  },
+  reducers: {}, //Specifically for local State actions, not data stuff?
+  extraReducers: (builder) => { //For post action (selectData) changes
     builder
-      .addCase(insertData.fulfilled, (state, action) => {
-        // handle the state change when insertData is fulfilled
+      .addCase(fetchProducts.pending, (state, action) => { // INTERESTING !!!!! // handle the state change when fetchProducts is pending
+          state.status = 'loading'
+        })
+
+      .addCase(fetchProducts.fulfilled, (state, action) => { // handle the state change when fetchProducts is fulfilled
+
+        state.productList = action.payload;
+
+        console.log(state.productList)
+        state.status = 'success'
       })
-      .addCase(selectData.fulfilled, (state, action) => {
-        // handle the state change when selectData is fulfilled
+
+      .addCase(fetchProducts.rejected, (state, action) => { // handle the state change when fetchProducts is rejected
+        
+        state.error = action.error.message
+
+        console.log(state.error)
+        state.status = 'failed'
       })
-      .addCase(updateData.fulfilled, (state, action) => {
-        // handle the state change when updateData is fulfilled
-      })
-      .addCase(deleteData.fulfilled, (state, action) => {
-        // handle the state change when deleteData is fulfilled
-      });
+
+      // .addCase(insertData.fulfilled, (state, action) => {
+      //   // handle the state change when insertData is fulfilled
+      // })
+      // .addCase(selectData.fulfilled, (state, action) => {
+      //   // handle the state change when selectData is fulfilled
+      // })
+      // .addCase(updateData.fulfilled, (state, action) => {
+      //   // handle the state change when updateData is fulfilled
+      // })
+      // .addCase(deleteData.fulfilled, (state, action) => {
+      //   // handle the state change when deleteData is fulfilled
+      // })
   },
 });
 
-export default firebaseSlice.reducer;
+
+export const firebaseActions = firebaseSlice.actions //For reducer use
+
+export default firebaseSlice //For store-index import
+// export default firebaseSlice.reducer;
+
+// export const insertData = createAsyncThunk(
+//   'firebase/insertData',
+//   async (data, thunkAPI) => {
+//     // Insert data into Firebase
+//     // You can dispatch other actions within this function
+//     // Return the result or throw an error
+//   }
+// );
+
+// export const selectData = createAsyncThunk(
+//   'firebase/selectData',
+//   async (data, thunkAPI) => {
+//     // Select data from Firebase
+//   }
+// );
+
+// export const updateData = createAsyncThunk(
+//   'firebase/updateData',
+//   async (data, thunkAPI) => {
+//     // Update data in Firebase
+//   }
+// );
+
+// export const deleteData = createAsyncThunk(
+//   'firebase/deleteData',
+//   async (data, thunkAPI) => {
+//     // Delete data from Firebase
+//   }
+// );
 
 /*
 When using Firebase in a React application, there are a few things you should keep in mind:
