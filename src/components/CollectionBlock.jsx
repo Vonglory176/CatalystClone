9a1a87852888cab606405b85d1886c8d7dde5753
Vonglory176 <SkylerGConley@gmail.com>
2024-01-03 16:12:34 -0800

    First attempt at CollectionBlock component. Works!

---
+++ src/components/CollectionBlock.jsx
@@ -0,0 +1,74 @@
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import ProductCard from "./ProductCard";
+import { getProductById } from "../hooks/getProductById";
+import { useEffect, useState } from "react";
+
+export default function CollectionBlock(
+    {collectionClasses, collectionLink,collectionFrameSrc,collectionCoverSrc,collectionCoverTitle,productIdArray,characterImageSrcArray}) {
+    const productList = useSelector(state => state.firebase.productList)
+    const status = useSelector(state => state.firebase.status)
+    const [collectionProducts, setCollectionProducts] = useState()
+    
+    useEffect(() => {
+        if(status === "success") {
+            let productCardArray = []
+    
+            for (let productId in productIdArray) {
+                productCardArray.push(
+                    <div className="grid__item" key={productId}>
+                        {<ProductCard product={getProductById(productList,productIdArray[productId])}/>}
+                    </div>
+                )
+            }
+            // console.log(productCardArray)
+    
+            setCollectionProducts(productCardArray)
+        }
+    },[status]) //Might be firing too soon?
+
+    //Could add code where not having a collection-cover ALSO hides component in small views?
+
+    return (
+        <section className={`section-block ${collectionClasses}`}>
+
+            {/* The background frame for larger views */}
+            <Link to={collectionLink} className={"collection-link"}>
+                <img src={collectionFrameSrc} alt="" className="featured-product-frame" />
+            </Link>
+
+            {characterImageSrcArray && //If no charImages, this is excluded   
+            <div className="featured-character">
+                <img src={characterImageSrcArray[0]} alt=""/>
+            </div>
+            }
+
+            <div className="grid collection">
+                <Link to={collectionLink} className={"collection-link"}></Link>
+
+                {collectionCoverTitle && //If no Title, this is excluded           
+                <div className="grid__item collection__cover">
+                    {/* <Link to={"/"} className={"collection-link"}></Link> */}
+                    <Link 
+                    to={collectionLink} 
+                    className={"collection-card"} 
+                    style={{backgroundImage: `url(${collectionCoverSrc})`}}
+                    >
+                        <div className="collection-card__meta">
+                            <p className="collection-card__title h1">{collectionCoverTitle}</p>
+                            <p className="collection-card__subtext">View all</p>
+                        </div>
+                    </Link>
+                </div>
+                }
+
+                {status==="success" && collectionProducts}
+                
+                {/* <div className="grid__item">
+                    {status==="success" && <ProductCard product={getProductById(productList,"battletech-clan-invasion")}/>}
+                </div> */}
+                
+            </div>
+        </section>
+    )
+}
