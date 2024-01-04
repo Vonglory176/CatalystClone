ffca663b2f64657b02edab0629df6bd63b0b38f3
Vonglory176 <SkylerGConley@gmail.com>
2024-01-03 16:12:56 -0800

    Included a second slideshow image

--- src/components/Slideshow.jsx
+++ src/components/Slideshow.jsx
@@ -1,19 +1,16 @@
 import { Carousel } from "react-bootstrap";
-import SlideshowTestImg from "../assets/banner/Rotating_Banner_-_BT_2_of_6_Templar_540x.avif"
+import SlideshowImg1 from "../assets/slideshow/Rotating_Banner_-_BT_2_of_6_Templar_900x.webp"
+import SlideshowImg2 from "../assets/slideshow/Rotating-Banner---SR-3-of-6-UrbanBrawl_1296x.webp"
 
 export default function Slideshow() {
     return (
     <Carousel controls={false} indicators={false} keyboard={false} touch={false}>
       <Carousel.Item>
-        <img src={SlideshowTestImg}/>
+        <img src={SlideshowImg1}/>
       </Carousel.Item>
 
       <Carousel.Item>
-        <img src={SlideshowTestImg}/>
-      </Carousel.Item>
-
-      <Carousel.Item>
-        <img src={SlideshowTestImg}/>
+        <img src={SlideshowImg2}/>
       </Carousel.Item>
     </Carousel>
     )
