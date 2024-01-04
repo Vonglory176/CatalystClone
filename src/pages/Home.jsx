0f7eb245a7bb67e217aebad5c2f24b45ab3aceca
Vonglory176 <SkylerGConley@gmail.com>
2024-01-03 16:14:18 -0800

    Revamped collection-blocks for new component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,40 +1,36 @@
 import ProductCard from "../components/ProductCard"
 import MailListSignUp from "../components/MailListSignUp"
 
-import gettingStartedBattletech from "../assets/getting-started/getting-started-battletech.webp"
-import gettingStartedShadowrun from "../assets/getting-started/getting-started-shadowrun.webp"
+import gettingStartedBattletech from "/src/assets/getting-started/getting-started-battletech.webp"
+import gettingStartedShadowrun from "/src/assets/getting-started/getting-started-shadowrun.webp"
 
-import fpFrame1 from "../assets/featured-products/featured-product-frame-1.svg"
-import fpFrame2 from "../assets/featured-products/featured-product-frame-2.svg"
-import fpFrame3 from "../assets/featured-products/featured-product-frame-3.svg"
-import fpFrame4 from "../assets/featured-products/featured-product-frame-4.svg"
+//Collection frames/covers
+import featuredFrame from "/src/assets/block-collection/frames/collection-frame-featured.svg"
+import featuredCover from "/src/assets/block-collection/covers/cicada-picture.webp"
+import battletechFrame from "/src/assets/block-collection/frames/collection-frame-battletech.svg"
+import battletechCover from "/src/assets/block-collection/covers/cicada-picture.webp"
+import shadowrunFrame from "/src/assets/block-collection/frames/collection-frame-shadowrun.svg"
+import shadowrunCover from "/src/assets/block-collection/covers/cicada-picture.webp"
+import tabletopFrame from "/src/assets/block-collection/frames/collection-frame-tabletop.svg"
+import tabletopCover from "/src/assets/block-collection/covers/cicada-picture.webp"
 
-import featuredCharacterVictor from "../assets/featured-products/featured-character-victor.webp"
-import featuredCharacterCombatMage from "../assets/featured-products/featured-character-combatMage.webp"
+//Collection Characters
+import characterCombatMage from "/src/assets/block-collection/characters/character-combatMage.webp"
+import characterVictor from "/src/assets/block-collection/characters/character-victor.webp"
 
-import saleButton from "../assets/featured-products/button-collection/collection-buttons-01.webp"
-import newArrivalsButton from "../assets/featured-products/button-collection/collection-buttons-02.webp"
-import freeDownloadButton from "../assets/featured-products/button-collection/collection-buttons-03.webp"
+import saleButton from "../assets/button-collection/collection-buttons-01.webp"
+import newArrivalsButton from "../assets/button-collection/collection-buttons-02.webp"
+import freeDownloadButton from "../assets/button-collection/collection-buttons-03.webp"
 import { useSelector } from "react-redux"
 import { getProductById } from "../hooks/getProductById"
 import { Link } from "react-router-dom"
+import CollectionBlock from "../components/CollectionBlock"
 
 export default function Home() {
     const productList = useSelector(state => state.firebase.productList)
     const status = useSelector(state => state.firebase.status)
 
-    /*
-        Need code for:
-
-        Collection link
-        ClassName
-        Banner Img Src/Alt
-        Collection name for collection-card__title
-        Product ID's
-
-        Have some kind of determination for stand/fea/new?
-    */
-
+    
     return (
         <div id="Home-Container">
             
@@ -53,19 +49,101 @@ export default function Home() {
                     <div className="featured-product-banner"></div>
                 </Link>
 
-                <section className="section-block"> {/* MAKE THIS A COMPONENT !!! */}
+                <CollectionBlock //FEATURED COLLECTION
+                collectionClasses={""}
+                collectionLink={"/collections/all"} //CHANGE TO HAVE FEATURED FILTER
+                collectionFrameSrc={featuredFrame}
+                collectionCoverSrc={featuredCover}
+                collectionCoverTitle={"Featured Products"}
+                productIdArray={[
+                    "battletech-clan-invasion",
+                    "battletech-reinforcements-clan-invasion",
+                    "battletech-battlemat-alien-worlds",
+                    "shadowrun-sixth-world-core-rulebook-city-edition-berlin",
+                    "i-would-fight-the-dragon",
+                ]}
+                characterImageSrcArray={""}
+                />
 
-                    {/* The background banner for larger views */}
+                <CollectionBlock //BATTLETECH COLLECTION
+                collectionClasses={"left"}
+                collectionLink={"/collections/battletech"}
+                collectionFrameSrc={battletechFrame}
+                collectionCoverSrc={battletechCover}
+                collectionCoverTitle={"Battletech"}
+                productIdArray={[
+                    "battletech-clan-invasion",
+                    "battletech-reinforcements-clan-invasion",
+                    "battletech-battlemat-alien-worlds",
+                    "battletech-miniature-pack-game-of-armored-combat",
+                ]}
+                characterImageSrcArray={[
+                    characterVictor,
+                ]}
+                />
+                <CollectionBlock //SHADOWRUN COLLECTION
+                collectionClasses={"right"}
+                collectionLink={"/collections/shadowrun"}
+                collectionFrameSrc={shadowrunFrame}
+                collectionCoverSrc={shadowrunCover}
+                collectionCoverTitle={"Shadowrun"}
+                productIdArray={[
+                    "shadowrun-sixth-world-core-rulebook-city-edition-berlin",
+                    "shadowrun-sixth-world-core-rulebook-city-edition-berlin",
+                    "shadowrun-sixth-world-core-rulebook-city-edition-berlin",
+                    "shadowrun-sixth-world-core-rulebook-city-edition-berlin",
+                ]}
+                characterImageSrcArray={[
+                    characterCombatMage,
+                ]}
+                />
+
+                <section className="collection-buttons-container">
+                    <div className="collection-buttons-wrapper">
+                        <Link to={"/"}>                            
+                            <img src={saleButton} alt="" className="collection-button" />
+                        </Link>
+                        <Link to={"/"}>                            
+                            <img src={newArrivalsButton} alt="" className="collection-button" />
+                        </Link>
+                        <Link to={"/"}>                            
+                            <img src={freeDownloadButton} alt="" className="collection-button" />
+                        </Link>
+                    </div>
+                </section>
+
+                <CollectionBlock //TABLETOP COLLECTION
+                collectionClasses={"left"}
+                collectionLink={"/collections/tabletop"}
+                collectionFrameSrc={tabletopFrame}
+                collectionCoverSrc={tabletopCover}
+                collectionCoverTitle={"Tabletop"}
+                productIdArray={[
+                    "i-would-fight-the-dragon",
+                    "i-would-fight-the-dragon",
+                    "i-would-fight-the-dragon",
+                    "i-would-fight-the-dragon",
+                ]}
+                characterImageSrcArray={""}
+                />                
+
+                <MailListSignUp/>
+            </div>
+        </div>                        
+    )
+}
+
+{/* 
+
+                <section className="section-block">
                     <Link to={"/collections/all"} className={"collection-link"}>
                         <img src={fpFrame1} alt="" className="featured-product-frame" />
                     </Link>
-
 
                     <div className="grid collection">
                         <Link to={"/"} className={"collection-link"}></Link>
 
                         <div className="grid__item collection__cover">
-                            {/* <Link to={"/"} className={"collection-link"}></Link> */}
                             <Link to={"/"} className={"collection-card"}>
                                 <div className="collection-card__meta">
                                     <p className="collection-card__title h1">Featured Products</p>
@@ -93,29 +171,6 @@ export default function Home() {
                     </div>
                 </section>
 
-                <section className="collection-buttons-container">
-                    <div className="collection-buttons-wrapper">
-                        <a href="!#">
-                            <img src={saleButton} alt="" className="collection-button" />
-                        </a>
-                        <a href="!#">
-                            <img src={newArrivalsButton} alt="" className="collection-button" />
-                        </a>
-                        <a href="!#">
-                            <img src={freeDownloadButton} alt="" className="collection-button" />
-                        </a>
-                    </div>
-                </section>
-
-                
-
-                <MailListSignUp/>
-            </div>
-        </div>                        
-    )
-}
-
-{/* 
                 <section className="section-block left">
                     <a href="!#" className="collection-link">
                         <img src={fpFrame2} alt="" className="featured-product-frame" />
