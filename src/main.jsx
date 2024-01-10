faeb2119cb2c02fc2942ef12851b8ccc77a5c1aa
Vonglory176 <SkylerGConley@gmail.com>
2024-01-10 11:03:27 -0800

    Setup App for Redux-Persist

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,17 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import "./style/index.css"
 import { Provider } from 'react-redux'
-import store from './store/index.jsx'
+import store, { persistor } from './store/index.jsx'
+import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename={"/"}> {/*CatalystClone*/}
-        <App/>
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter basename={"/"}> {/*CatalystClone*/}
+          <App/>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
 )
--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,14 +1,36 @@
-import { configureStore } from "@reduxjs/toolkit"
+import { combineReducers, configureStore } from "@reduxjs/toolkit"
+import { persistStore, persistReducer } from "redux-persist"
+import storage from "redux-persist/lib/storage"
 import authSlice from "./auth-slice"
 import productsSlice from "./products-slice"
 import cartSlice from "./cart-slice"
 
-const store = configureStore({
-    reducer: {
-        auth: authSlice.reducer,
-        products: productsSlice.reducer,
-        cart: cartSlice.reducer,
-    }
+const persistConfig = {
+    key: "root",
+    storage,
+    whitelist: ['auth', 'cart']
+}
+
+const rootReducer = combineReducers({
+    auth: authSlice.reducer,
+    products: productsSlice.reducer,
+    cart: cartSlice.reducer,
 })
 
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const store = configureStore({
+    reducer: persistedReducer
+})
+
+export const persistor = persistStore(store)
+
 export default store
+
+// const store = configureStore({
+//     reducer: {
+//         auth: authSlice.reducer,
+//         products: productsSlice.reducer,
+//         cart: cartSlice.reducer,
+//     }
+// })
