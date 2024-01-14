8e2194fc377737b2e26d8f20c22c5f650ae7b56f
Vonglory176 <SkylerGConley@gmail.com>
2024-01-13 18:19:14 -0800

    ProgressiveImage tests w/Slideshow Images

--- src/components/Slideshow.jsx
+++ src/components/Slideshow.jsx
@@ -1,16 +1,62 @@
 import { Carousel } from "react-bootstrap";
-import SlideshowImg1 from "../assets/slideshow/Rotating_Banner_-_BT_2_of_6_Templar_900x.webp"
-import SlideshowImg2 from "../assets/slideshow/Rotating-Banner---SR-3-of-6-UrbanBrawl_1296x.webp"
+
+import templarImg1 from "../assets/slideshow/Rotating_Banner_Templar_540x.avif"
+import templarImg2 from "../assets/slideshow/Rotating_Banner_Templar_720x.webp"
+import templarImg3 from "../assets/slideshow/Rotating_Banner_Templar_900x.webp"
+
+import urbanBrawl1 from "../assets/slideshow/Rotating-Banner_UrbanBrawl_1296x.webp"
+import urbanBrawl2 from "../assets/slideshow/Rotating-Banner_UrbanBrawl_1512x.webp"
+
+import ProgressiveImage from "react-progressive-image";
+import placeholderImage from "../assets/placeholder.png"
 
 export default function Slideshow() {
     return (
     <Carousel controls={false} indicators={false} keyboard={false} touch={false}>
+
+      {/* TEMPLAR SLIDE */}
       <Carousel.Item>
-        <img src={SlideshowImg1}/>
+        <ProgressiveImage 
+        src={templarImg1}
+        srcSetData={{
+          srcSet:`
+          ${templarImg1} 540w, 
+          ${templarImg2} 720w, 
+          ${templarImg3} 900w`
+        }}
+        placeholder={placeholderImage}
+        >
+            {(src, loading, srcSetData) =>
+              <img
+              className={loading? "imgLoading":"imgLoaded"}
+              src={src}
+              srcSet={srcSetData.srcSet}
+              alt={""}
+              /> 
+            }
+        </ProgressiveImage>
       </Carousel.Item>
 
+      {/* URBAN-BRAWL SLIDE */}
       <Carousel.Item>
-        <img src={SlideshowImg2}/>
+        <ProgressiveImage 
+        src={templarImg1}
+        srcSetData={{
+          srcSet:`
+          ${urbanBrawl1} 1296w, 
+          ${urbanBrawl2} 1512w`
+        }}
+        placeholder={placeholderImage}
+        >
+            {(src, loading, srcSetData) =>
+              <img
+              className={loading? "imgLoading":"imgLoaded"}
+              src={src}
+              srcSet={srcSetData.srcSet}
+              alt={""}
+              /> 
+            }
+        </ProgressiveImage>
       </Carousel.Item>
     </Carousel>
     )
