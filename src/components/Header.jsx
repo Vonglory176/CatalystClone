5ffbf71f7152cd1b6d9f86e8bb2ecff28e1f78d1
Vonglory176 <SkylerGConley@gmail.com>
2024-01-18 17:03:31 -0800

    Drop-down & Sticky Header code, needs cleanup

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useState } from 'react';
 import { Offcanvas } from 'react-bootstrap';
 
@@ -6,28 +6,64 @@ import { useSelector } from "react-redux";
 
 import catalystLogo from "../assets/logo-catalyst2_450x.webp"
 import { Link } from 'react-router-dom';
+import { useInView } from 'react-intersection-observer';
 // import auth from "../store/auth-slice";
 
 export default function Header() {
     const isLoggedIn = useSelector(state=> state.auth.isLoggedIn)
 
     //Offcanvas/Sidebar
-    const [show, setShow] = useState(false);
-    const handleClose = () => setShow(false);
-    const toggleShow = () => setShow((s) => !s);
+    const [showSidebar, setShowSidebar] = useState(false);
+    const handleClose = () => setShowSidebar(false);
+    const toggleShowSidebar = () => setShowSidebar((s) => !s);
     
+    //For dropdown menu in Offcanvas/Sidebar
     const [showUniverseSubMenu, setUniverseSubMenu] = useState(true)
     const toggleUniverseSubMenu = () => setUniverseSubMenu((s) => !s);
     const cartItems = useSelector(state => state.cart.cartItemList)
+
+    //Sticky-Header Observer
+    const [ref, inView] = useInView({
+        threshold: 0
+    })
+
+    // State to store whether the viewport is wide
+    const [isWideViewport, setIsWideViewport] = useState(window.innerWidth >= 750);
+
+    // Update isWideViewport when the window is resized
+    useEffect(() => {
+        const handleResize = () => {
+            setIsWideViewport(window.innerWidth >= 750);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        // Clean up the event listener when the component is unmounted
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    const storeButtons = ( //For easy sharing between upper/sticky
+    <div className="store-btns">
+
+        <i className="fa-solid fa-magnifying-glass store-btns__search fa-lg"></i>
+
+        <Link to={"/cart"} className={"store-btns__link store-btns__cart-link"}>
+            <i className="fa-solid fa-cart-shopping store-btns__cart fa-lg"></i>
+            {/* Cart-item idicator */}
+            {cartItems.length > 0? <i className="fa-solid fa-circle store-btns__cart-notification fa-xs" style={{color:"#c2ca20"}}></i> : ""}
+        </Link>
+
+    </div>)
 
     return (
         <header className="header">
 
             {/* Header Upper */}
-
             <div className="header__upper">
                 <div className="nav-btn">
-                    <button onClick={toggleShow}>
+                    <button onClick={toggleShowSidebar}>
                         <i className="fa-solid fa-bars nav-btn__burger fa-lg"></i>
                         {/* <i className="fa-solid fa-xmark"></i> */}
                     </button>
@@ -39,36 +75,43 @@ export default function Header() {
                     </Link>
                 </div>
 
-                <div className="store-btns">
+                {storeButtons}
 
-                    {/* <i className="fa-solid fa-magnifying-glass store-btns__search fa-lg"></i> */}
-
-                    <Link to={"/cart"} className={"store-btns__link"}>
-                        <i className="fa-solid fa-cart-shopping store-btns__cart fa-lg"></i>
-                    </Link>
-
-                    {/* Cart has items idicator */}
-                    {cartItems.length > 0? <i className="fa-solid fa-circle store-btns__cart-notification fa-xs" style={{color:"#c2ca20"}}></i> : ""}
-                </div>
             </div>
 
             {/* Header Sticky */}
+            <div className="header__sticky-wrapper" ref={ref}>
+                <div className={`header__sticky ${inView || !isWideViewport ? '' : 'sticky'}`}>
+                    <div className="universe-dropdown dropdown">
+                        {/* <Link to={"/collections/battletech"} reloadDocument>
+                            <button className="header__sticky-dropdown-button btn">Universe</button>
+                        </Link> */}
 
-            <div className="header__sticky">
-                <div className="header__sticky-dropdown">
-                    <Link to={"/collections/battletech"} reloadDocument>
-                        <button className="header__sticky-dropdown-button btn">Universe</button>
-                    </Link>
-                </div>
-                <div className="header__sticky-links">
-                    <Link to={"/account"} className={"account-link"}>Account</Link>
-                    {isLoggedIn && <Link to={"/account/logout"} className={"logout-link"}>Log Out</Link>}
+                        <button className="universe-dropdown__button dropdown-button">Universe<i class="fa-solid fa-caret-down"></i></button>
+                        {/* {isFocused && ( */}
+                            <div className="universe-dropdown__content dropdown-content">
+                                <Link to={"/collections/all"}><h1>UNIVERSE</h1></Link>
+                                <Link to={"/collections/battletech"}>Battletech</Link>
+                                <Link to={"/collections/shadowrun"}>Shadowrun</Link>
+                                <Link to={"/collections/other"}>Other</Link>
+                            </div>
+                        {/* )} */}
+                    </div>
+
+                    {inView? //Determining what buttons to have
+
+                    <div className="header__sticky-links">
+                        <Link to={"/account"} className={"account-link"}>Account</Link>
+                        {isLoggedIn && <Link to={"/account/logout"} className={"logout-link"}>Log Out</Link>}
+                    </div>
+
+                    : storeButtons}
+
                 </div>
             </div>
 
             {/* Side Nav */}
-
-            <Offcanvas show={show} onHide={handleClose} backdrop={true}> {/*scroll={true}*/}
+            <Offcanvas show={showSidebar} onHide={handleClose} backdrop={true}> {/*scroll={true}*/}
 
                 <Offcanvas.Header > {/* closeButton */}
                     {/* <input type="text" className="offcanvas__searchbar"/> */}
