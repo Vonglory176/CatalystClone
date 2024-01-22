a908840d84565aa1e8cb7bde9490149a362907d9
Vonglory176 <SkylerGConley@gmail.com>
2024-01-21 17:36:49 -0800

    First functional stab at legit searchbar component

---
+++ src/components/Searchbar.jsx
@@ -0,0 +1,103 @@
+import { useEffect, useState } from "react"
+import { useSelector } from "react-redux"
+import Fuse from 'fuse.js'
+import { Link } from "react-router-dom"
+import ProductSearchbarResult from "./ProductSearchbarResult"
+
+export default function Searchbar() {
+    const productList = useSelector(state => state.products.productList)
+    const [searchTerm, setSearchTerm] = useState('')
+    const [searchResults, setSearchResults] = useState('')
+    const [searchResultsHtml, setSearchResultsHtml] = useState('')
+    const [fuse, setFuse] = useState(null)
+
+    //Area of search (All products have been combined into a single array as seen below)
+    const options = {
+        keys: [
+            'name',
+            'universe',
+            'description'
+        ],
+        includeScore: true,
+        threshold: 0.3
+      }
+    
+    //Initializing the Fuse
+    useEffect(() => {
+        if (productList) {
+            let tempProductList = []        
+            Object.keys(productList).forEach(category => { //For each Product Category
+                Object.values(productList[category]).forEach(product => tempProductList.push(product))//For each Product in that Category
+            })
+
+            setFuse(new Fuse(tempProductList, options))
+        }
+    }, [productList])
+
+    //Updating current search-results (BEFORE SUBMIT)
+    const handleSearchInputChange = (event) => {
+
+        const query = event.target.value //Getting/Setting Query
+        setSearchTerm(query)
+
+        const results = fuse.search(query) //Getting/Setting Results
+        setSearchResults(results)
+
+        // console.log(query)
+        // console.log(fuse)
+        console.log(results)
+    }
+
+    //Printing search-result HTML
+    useEffect(() => {
+        let tempSearchResultsHtml = []
+        // let tempSearchResultContainerHtml
+
+        if (searchResults) {
+            //Get first three results/items
+            for (let i = 0; i < 3 && i < searchResults.length; i++) {
+                const product = searchResults[i].item
+                tempSearchResultsHtml.push(ProductSearchbarResult(product))
+            }
+                //Adding the "Products" header if products were found
+                if (tempSearchResultsHtml.length > 0) tempSearchResultsHtml.unshift(
+                <div className="searchbar-results-header">Products</div>
+                )
+            
+            // searchResults
+            //Print length/link to the rest
+
+            // tempSearchResultContainerHtml = (
+                // <div className="searchbar-results-container">
+                //     {tempSearchResultsHtml}
+                // </div>
+            // )
+
+        }
+
+        setSearchResultsHtml(tempSearchResultsHtml)
+    }, [searchResults])
+    
+    //Sending results/routing to Search Page (AFTER SUBMIT)
+    const handleSearch = (event) => {
+        event.preventDefault()
+        onSearch(searchTerm)
+        console.log(results)
+    }
+
+    return (
+        <form onSubmit={handleSearch} className="searchbar">
+            <input
+            type="text"
+            placeholder="Search..."
+            value={searchTerm}
+            onChange={(e) => handleSearchInputChange(e)}
+            />
+
+            <div className="searchbar-results-container">
+                {searchResultsHtml}
+            </div>
+            {/* <button type="submit">Search</button> */}
+        </form>
+    )
+}
