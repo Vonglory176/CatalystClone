9acea1a19a5e681ae7c4706034bb9d3060a72613
Vonglory176 <SkylerGConley@gmail.com>
2024-02-01 10:50:35 -0800

    Swapped Router basename for VITE_PUBLIC_URL env

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename={"/CatalystClone"}> {/**/}
+        <BrowserRouter basename={`${import.meta.env.VITE_PUBLIC_URL}`}> {/*"/CatalystClone"*/}
           <App/>
         </BrowserRouter>
       </PersistGate>
