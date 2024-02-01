eca1af3f6f74d75d796ea29076c28ed0ae417774
Vonglory176 <SkylerGConley@gmail.com>
2024-01-31 17:38:54 -0800

    New default route. Change back for dev work!!!

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename={"/"}> {/*CatalystClone*/}
+        <BrowserRouter basename={"/CatalystClone"}> {/**/}
           <App/>
         </BrowserRouter>
       </PersistGate>
