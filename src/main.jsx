7041d7629d9a81fa659aadbfd1292a54b8a9fab3
Vonglory176 <SkylerGConley@gmail.com>
2024-02-01 17:22:24 -0800

    Swapped (sadly) to hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 import "./style/index.css"
 import { Provider } from 'react-redux'
 import store, { persistor } from './store/index.jsx'
@@ -11,9 +11,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename={`${import.meta.env.VITE_PUBLIC_URL}`}> {/*"/CatalystClone"*/}
+        <HashRouter basename={`${import.meta.env.VITE_PUBLIC_URL}`}> {/*"/CatalystClone"*/}
           <App/>
-        </BrowserRouter>
+        </HashRouter>
       </PersistGate>
     </Provider>
   </React.StrictMode>,
