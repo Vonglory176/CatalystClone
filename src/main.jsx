b7956bd6cf2e4deae60900f6bb86ff36b14d51dc
Vonglory176 <SkylerGConley@gmail.com>
2024-02-01 17:53:09 -0800

    Small changes to work with HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <HashRouter basename={`${import.meta.env.VITE_PUBLIC_URL}`}> {/*"/CatalystClone"*/}
+        <HashRouter> {/*basename={`${import.meta.env.VITE_PUBLIC_URL}`} "/CatalystClone"*/}
           <App/>
         </HashRouter>
       </PersistGate>
