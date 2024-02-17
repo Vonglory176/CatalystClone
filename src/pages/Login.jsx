4620d6572b39b8f52fac11833292b9f4f7de724f
Vonglory176 <SkylerGConley@gmail.com>
2024-02-16 18:14:11 -0800

    Built and then moved login code to Redux auth-slice

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,21 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { Link, useNavigate } from "react-router-dom";
 
-import { useDispatch } from "react-redux"
-import { authActions } from "../store/auth-slice.jsx"
+import { useDispatch, useSelector } from "react-redux"
+import { loginWithUserDetails } from "../store/auth-slice.jsx"
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth"
 
 export default function Login() {
+    const isLoggedIn = useSelector(state=> state.auth.isLoggedIn)
     const dispatch = useDispatch()
     const navigate = useNavigate()
+
+    //Routing to Account page if user is logged in
+    useEffect(() => {
+        if (isLoggedIn) {
+            navigate("/account", {replace:true})
+        }
+    }, [isLoggedIn])
 
     const handleLoginSubmit = async (e) => {
         e.preventDefault() //Prevents refresh/data-sending
@@ -15,22 +23,23 @@ export default function Login() {
         const email = e.target.elements['customer[email]'].value
         const password = e.target.elements['customer[password]'].value
         
-        try {
+        // try {
             // Use Firebase Authentication to sign in
-            const auth = getAuth()
+            // const auth = getAuth()
 
             // Checking for account with matching credentials
-            const response = await signInWithEmailAndPassword(auth, email, password)
-            console.log('Login successful!', response)
+            // await signInWithEmailAndPassword(auth, email, password)
+            //const response = await signInWithEmailAndPassword(auth, email, password)
+            //console.log('Login successful!', response)
 
-            // Setting login status in Redux and routing to Account page
-            dispatch(authActions.login())
-            navigate("/account")
-        } 
-        catch (error) { 
-            console.error('Login failed:', error.code, error.message)
-        }
-    }    
+            // Getting user-details and setting login status in Redux
+            // Auto routes to Account page w/useEffect above
+            dispatch(loginWithUserDetails({email, password}))
+        // } 
+        // catch (error) { 
+        //     console.error('Login failed:', error.code, error.message)
+        // }
+    }
 
     const [passwordRecovery, setPasswordRecovery] = useState(false)
     const togglePasswordRecovery = () => setPasswordRecovery((r) => !r)
