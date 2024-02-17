c17c2cb425ab2a114011c7f23e85e3e0879bb1e2
Vonglory176 <SkylerGConley@gmail.com>
2024-02-16 18:13:06 -0800

    Heavily flushed out Account usage, now printing details like User Address

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,15 +1,23 @@
 import { useEffect } from "react"
 import { useSelector } from "react-redux"
-import { Link, Navigate, useNavigate } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 
 export default function Account() {
-    const isLoggedIn = useSelector(state=> state.auth.isLoggedIn)
-
     //If not logged in, redirect to Login page ("replace" so back button works)
-    if (!isLoggedIn) return <Navigate to="login" replace/>
+    const isLoggedIn = useSelector(state => state.auth.isLoggedIn)
+    const navigate = useNavigate()
+    // if (!isLoggedIn) return <Navigate to="login" replace/>
+
+    useEffect(() => {
+        if (!isLoggedIn) navigate("login", {replace:true})
+    }, [isLoggedIn])
+
+    const user = useSelector(state => state.auth.user)
+    // const {orders, addresses} = useSelector(state => state.auth.user)
+    const address = user? user.addresses.find(address => address.isDefaultAddress) : {}
 
     return (
-        <div id="Account-Container">
+        <div id="Addresses-Container">
             <div className="content-block">
                 <h1>My Account</h1>
                 <p className="account__continue">
@@ -20,12 +28,33 @@ export default function Account() {
                 </p>
 
                 <h2>Order History</h2>
-                <p>You haven't placed any orders yet.</p>
+                {user && user.orders?
+                    user.orders : //CHANGE TO ITERATE AND PRINT HISTORY
+                    <p>You haven't placed any orders yet.</p>
+                }
             </div>
+
             <div className="content-block">
                 <h3>Account Details</h3>
-                <p>First Last <br/> Country</p>
-                <a href="!#">View Addreses (X)</a>
+                {(user && user.addresses.length > 0) && 
+                <div className="address-container">
+                    <p>
+                        {address.firstName} {address.lastName} {(address.firstName || address.lastName) && <br/>}
+                        {address.company} {address.company && <br/>}
+                        {address.country} {address.country && <br/>}
+                        {address.address1} {address.address1 && <br/>}
+                        {address.address2} {address.address2 && <br/>}
+                        {address.city} {address.province} {address.postalCode} {(address.city || address.province || address.postalCode) && <br/>}
+                    </p>
+                    {/* <p>{address.firstName} {address.lastName}</p>
+                    <p>{address.country}</p>
+                    <p>{address.address1}</p>
+                    <p>{address.address2}</p>
+                    <p>{address.city} {address.province} {address.postalCode}</p> */}
+                </div>
+                
+                }
+                <Link to={"addresses"} className="addresses-link">View Addreses ({user? user.addresses.length : 0})</Link>
             </div>
         </div>
     )
