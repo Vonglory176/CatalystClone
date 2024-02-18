327690c82709ef82c8a4692f4d25ff0faf5ba349
Vonglory176 <SkylerGConley@gmail.com>
2024-02-17 17:37:42 -0800

    Removed console.log for response

--- src/hooks/getUserCountry.jsx
+++ src/hooks/getUserCountry.jsx
@@ -3,7 +3,7 @@ export const fetchUserCountry = async () => {
       const response = await fetch('https://ipapi.co/json/')
       if (response.ok) {
         const data = await response.json()
-        console.log(data.country_name) // Logs the country name
+        // console.log(data.country_name) // Logs the country name
         return data.country_name
       } else {
         console.error('Failed to fetch country')
