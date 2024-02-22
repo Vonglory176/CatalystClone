7c155e90b06f44d94aa5e76fd817c2d6564909bf
Vonglory176 <SkylerGConley@gmail.com>
2024-02-21 19:09:24 -0800

    New page for CheckoutSuccess. Needs work to load order data

---
+++ src/pages/CheckoutSuccess.jsx
@@ -0,0 +1,21 @@
+import {useEffect} from 'react'
+import {useSearchParams} from 'react-router-dom'
+
+export default function CheckoutSuccess() {
+    const [searchParams, setSearchParams] = useSearchParams();
+    
+    useEffect(() => {
+        // Get the sessionId parameter from the URL
+        const sessionId = searchParams.get('sessionId')
+        console.log(sessionId)
+
+        const orderRed = ref(getDatabase(),)        
+    }, [searchParams])
+
+    return (
+        <div>
+            <h1>Thank you for your purchase!</h1>
+        </div>
+    )
+}
+
