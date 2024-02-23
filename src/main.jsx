aee97f49a4200bc5aa02999be53d75f986378d92
Vonglory176 <SkylerGConley@gmail.com>
2024-02-22 18:56:32 -0800

    Trying out new app structuring w/"onAuthStateChanged"

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,66 @@
 import React from 'react'
+import { useEffect } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter, HashRouter } from 'react-router-dom'
 import "./style/index.css"
-import { Provider } from 'react-redux'
 import store, { persistor } from './store/index.jsx'
 import { PersistGate } from 'redux-persist/integration/react'
-// import { getAuth } from "firebase/auth"
+import { getAuth, onAuthStateChanged } from "firebase/auth"
+import { fetchUserDetails, authActions } from './store/auth-slice.jsx'
+import { Provider, useDispatch } from 'react-redux'
 
 import firebaseApp from '../functions/firebaseConfig'
 
+const auth = getAuth()
+
+const AppWrapper = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        // User is signed in
+        console.log('User is signed in:', user);
+        // Dispatch an action to fetch user details from the database
+        dispatch(fetchUserDetails());
+      } else {
+        // User is signed out
+        console.log('User is signed out');
+        // Dispatch an action to update the store on sign-out
+        dispatch(authActions.logout());
+      }
+    });
+
+    // Cleanup subscription on unmount
+    return () => unsubscribe()
+  }, [dispatch]); // Add dispatch to the dependency array
+
+  return <App/>
+}
+        
+ReactDOM.createRoot(document.getElementById('root')).render(
+  // <React.StrictMode>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter basename={`${import.meta.env.VITE_PUBLIC_URL}`}> {/*basename={`${import.meta.env.VITE_PUBLIC_URL}`} "/CatalystClone"*/}
+        <AppWrapper/>
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>
+  // </React.StrictMode>,
+)
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   // <React.StrictMode></React.StrictMode>,
+//   <Provider store={store}>
+//     <PersistGate loading={null} persistor={persistor}>
+//       <BrowserRouter basename={`${import.meta.env.VITE_PUBLIC_URL}`}> {/*basename={`${import.meta.env.VITE_PUBLIC_URL}`} "/CatalystClone"*/}
+//         <App/>
+//       </BrowserRouter>
+//     </PersistGate>
+//   </Provider>,
+// )
 
 // import { initializeApp } from "firebase/app"
 // import { getDatabase } from 'firebase/database'
@@ -27,17 +78,5 @@ import firebaseApp from '../functions/firebaseConfig'
 
 // // INITIALIZE FIREBASE
 // const app = initializeApp(firebaseConfig)
-// const auth = getAuth()
 // const analytics = getAnalytics(app)
 // const db = getDatabase()
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode></React.StrictMode>,
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter basename={`${import.meta.env.VITE_PUBLIC_URL}`}> {/*basename={`${import.meta.env.VITE_PUBLIC_URL}`} "/CatalystClone"*/}
-        <App/>
-      </BrowserRouter>
-    </PersistGate>
-  </Provider>,
-)
