86ae7e323edcb7277363ed34bb23499d9503b458
Vonglory176 <SkylerGConley@gmail.com>
2024-02-23 18:11:37 -0800

    Added rough order history printing and minor styling

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,19 +1,10 @@
 import { useEffect, useState } from "react"
 import { useSelector } from "react-redux"
 import { Link, useNavigate } from "react-router-dom"
+import OrderHistory from "../components/OrderHistory"
 
 export default function Account() {
-    //If not logged in, redirect to Login page ("replace" so back button works)
-    // const isLoggedIn = useSelector(state => state.auth.isLoggedIn)
-    // const navigate = useNavigate()
-    // if (!isLoggedIn) return <Navigate to="login" replace/>
-
-    // useEffect(() => {
-    //     if (!isLoggedIn) navigate("login", {replace:true})
-    // }, [isLoggedIn])
-
     const user = useSelector(state => state.auth.user)
-
     const [address, setAddress] = useState(false)
 
     useEffect(() => {
@@ -26,45 +17,55 @@ export default function Account() {
     }, [user])
 
     return (
-        <div id="Addresses-Container">
-            <div className="content-block">
-                <h1>My Account</h1>
-                <p className="account__continue">
-                    <Link to={"/"} className={"btn"}>My Downloadable Files</Link> {/* Note "All" */}
-                </p>
-                {/* <p className="account__continue">
-                    <Link to={"/"} className={"btn"}>Manage My Membership</Link>
-                </p> */}
+        <div id="Account-Container">
+            <h1 className="page-title">My Account</h1>
 
-                <h2>Order History</h2>
-                {user && user.orders?
-                    user.orders : //CHANGE TO ITERATE AND PRINT HISTORY
-                    <p>You haven't placed any orders yet.</p>
-                }
-            </div>
+            <div className="content-wrapper">
 
-            <div className="content-block">
-                <h3>Account Details</h3>
-                {address && 
-                <div className="address-container">
-                    <p>
-                        {address.firstName} {address.lastName} {(address.firstName || address.lastName) && <br/>}
-                        {address.company} {address.company && <br/>}
-                        {address.country} {address.country && <br/>}
-                        {address.address1} {address.address1 && <br/>}
-                        {address.address2} {address.address2 && <br/>}
-                        {address.city} {address.province} {address.postalCode} {(address.city || address.province || address.postalCode) && <br/>}
+                <div id="Account-Details__Main-container" className="content-block">
+                    <p className="account__continue">
+                        <Link to={"/"} className={"btn"}>My Downloadable Files</Link> {/* Note "All" */}
                     </p>
-                    {/* <p>{address.firstName} {address.lastName}</p>
-                    <p>{address.country}</p>
-                    <p>{address.address1}</p>
-                    <p>{address.address2}</p>
-                    <p>{address.city} {address.province} {address.postalCode}</p> */}
+                    {/* <p className="account__continue">
+                        <Link to={"/"} className={"btn"}>Manage My Membership</Link>
+                    </p> */}
+
+                    <OrderHistory/>
+                </div>
+
+                <div id="Account-Details__Secondary-Container" className="content-block">
+                    <h3>Account Details</h3>
+                    {address && 
+                    <div className="address-container">
+                        <p>
+                            {address.firstName} {address.lastName} {(address.firstName || address.lastName) && <br/>}
+                            {address.company} {address.company && <br/>}
+                            {address.country} {address.country && <br/>}
+                            {address.address1} {address.address1 && <br/>}
+                            {address.address2} {address.address2 && <br/>}
+                            {address.city} {address.province} {address.postalCode} {(address.city || address.province || address.postalCode) && <br/>}
+                        </p>
+                        {/* <p>{address.firstName} {address.lastName}</p>
+                        <p>{address.country}</p>
+                        <p>{address.address1}</p>
+                        <p>{address.address2}</p>
+                        <p>{address.city} {address.province} {address.postalCode}</p> */}
+                    </div>
+                    
+                    }
+                    <Link to={"addresses"} className="addresses-link">View Addreses ({address? user.addresses.length : 0})</Link>
                 </div>
                 
-                }
-                <Link to={"addresses"} className="addresses-link">View Addreses ({address? user.addresses.length : 0})</Link>
             </div>
         </div>
     )
 }
+
+//If not logged in, redirect to Login page ("replace" so back button works)
+    // const isLoggedIn = useSelector(state => state.auth.isLoggedIn)
+    // const navigate = useNavigate()
+    // if (!isLoggedIn) return <Navigate to="login" replace/>
+
+    // useEffect(() => {
+    //     if (!isLoggedIn) navigate("login", {replace:true})
+    // }, [isLoggedIn])
