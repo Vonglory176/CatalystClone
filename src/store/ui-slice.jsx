77c96848e5277b42fa11e9f2ce8db6d2d4ca5928
Vonglory176 <SkylerGConley@gmail.com>
2024-02-28 19:40:26 -0800

    New slice for Notification display

---
+++ src/store/ui-slice.jsx
@@ -0,0 +1,53 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+export const showAndHideNotification = createAsyncThunk(
+    'ui/showAndHideNotification',
+    async (notification, thunkAPI) => {
+        // Dispatch the action to show the notification
+        thunkAPI.dispatch(uiActions.showNotification(notification));
+
+        // Wait for 5 seconds before automatically hiding the notification
+        setTimeout(() => {
+            thunkAPI.dispatch(uiActions.closeNotification());
+        }, 5000);
+    }
+)
+
+//(Reducers specify how the application's state changes in response to actions sent to the store.)
+const uiSlice = createSlice({
+  name: 'ui',
+  initialState: {
+    notification: {
+        message: '',
+        linkMessage: '',
+        link: '',
+        type: '',
+        open: false,
+    }
+  },
+
+  reducers: {
+    showNotification(state, action) {
+        console.log("Notification being set!")
+        console.log(action.payload)
+
+        state.notification = {
+            message: action.payload.message,
+            linkMessage: action.payload.linkMessage,
+            link: action.payload.link,
+            type: action.payload.type,
+            open: true,
+        }
+    },
+
+    closeNotification(state, action) {
+        state.notification.open = false
+    },
+
+  },
+
+})
+
+export const uiActions = uiSlice.actions //For reducer use
+
+export default uiSlice //For store-index import
