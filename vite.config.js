55f420ee4018d2d8437235e6799c204b608be1c8
Vonglory176 <SkylerGConley@gmail.com>
2024-03-10 19:24:01 -0700

    Installed "VitePWA" to allow service-worker use on deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      // Enable the injectManifest mode
+      injectManifest: {
+        swSrc: '/service-worker.js',
+      },
+    })
+  ],
   base: "/"
 })
