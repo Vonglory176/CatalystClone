f48c2997c374cc4a7ade0f77fd5ee69d9d1e3fc7
Vonglory176 <SkylerGConley@gmail.com>
2024-03-11 18:48:46 -0700

    Variable typo fix

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -42,9 +42,9 @@ export default function MainLayout() {
             Object.keys(backgrounds).forEach(key => {
                 const low = new Image();
                 low.src = backgrounds[key].low
-                
+
                 if (key === id) {
-                    const low = new Image();
+                    const high = new Image();
                     high.src = backgrounds[key].high
                 }
             })
